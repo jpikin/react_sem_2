581c058565d01ef4bb5755720d83d3fa88954a9a
Pikin <pikin@bk.ru>
2024-10-22 16:46:56 +0500

    hw added

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Counter from './components/Counter';
 import TextInput from './components/TextInput';
 import TodoList from './components/TodoList';
 import Timer from './components/Timer';
+import CommentsList from './components/CommentsList';
 
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
       <TextInput />
       <TodoList />
       <Timer />
+      <CommentsList/>
     </div>
   );
 }
---
+++ src/components/CommentsList.js
@@ -0,0 +1,37 @@
+import { useState } from "react";
+
+
+
+function CommentsList() {
+    
+    const [list, setList] = useState([
+        { id: 1, text: "Это первый комментарий" },
+        { id: 2, text: "Это второй комментарий" },
+        { id: 3, text: "Это третий комментарий" }
+    ]);
+
+    const [text, setText] = useState('');
+
+    const addComment = () => {
+        setList();
+        setText('');
+    }
+
+    function removeComment(num)  {
+        setList(prevList => prevList.filter(comment => comment.id !== num));
+    }
+    
+    return ( 
+        <>
+        <p>Home work</p>
+
+        <ul>
+                {list.map(el => 
+                    <li key={el.id}>{el.text} <button onClick={() => removeComment(el.id)}>Удалить комментарий</button></li>
+                )}
+            </ul>
+        </>
+     );
+}
+
+export default CommentsList;
